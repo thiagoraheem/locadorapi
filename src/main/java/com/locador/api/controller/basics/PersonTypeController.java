7524f3c8b187901eed6c1f50f9fe1e53a913093b
thiagoraheem <thiago.raheem@gmail.com>
2025-05-14 08:39:28 -0400

    Atualização de configurações Alteração na nomenclatura do endpoint persontype

--- src/main/java/com/locador/api/controller/basics/PersonTypeController.java
+++ src/main/java/com/locador/api/controller/basics/PersonTypeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/personType")
+@RequestMapping("/api/persontype")
 @CrossOrigin
 public class PersonTypeController {
 
