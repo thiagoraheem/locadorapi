857ec53c16196f661a3b595d216bad228bd03c42
joaoataide38 <jmendesataide@gmail.com>
2025-05-18 03:29:54 -0300

    Criar Repository, Service e Controller para Product

--- src/main/java/com/locador/api/repository/basics/ProductRepository.java
+++ src/main/java/com/locador/api/repository/basics/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.locador.api.repository.basics;
 
-public interface ProductRepository {
+import com.locador.api.model.basics.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
