88994c479a29e0977d5af8942938fb64dc9bba84
joaoataide38 <jmendesataide@gmail.com>
2025-05-19 13:00:57 -0300

    Criado Repository, Controller e Service da classe ProductSerial

---
+++ src/main/java/com/locador/api/repository/basics/ProductSerialRepository.java
@@ -0,0 +1,8 @@
+package com.locador.api.repository.basics;
+
+import com.locador.api.model.basics.ProductRealEstate;
+import com.locador.api.model.basics.ProductSerial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductSerialRepository extends JpaRepository<ProductSerial, Integer> {
+}
