fb34f2766535bf6a3d293f817617f9bf3fdeca18
joaoataide38 <jmendesataide@gmail.com>
2025-05-19 11:28:53 -0300

    Adicionado Repository, Service e Controller da classe ProductRealEstate

---
+++ src/main/java/com/locador/api/controller/basics/ProductRealEstateController.java
@@ -0,0 +1,63 @@
+package com.locador.api.controller.basics;
+
+import com.locador.api.model.basics.ProductRealEstate;
+import com.locador.api.service.basics.ProductRealEstateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productrealestate")
+@CrossOrigin
+public class ProductRealEstateController {
+
+    @Autowired
+    private ProductRealEstateService productRealEstateService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductRealEstate>> getAll() {
+        try {
+            return ResponseEntity.ok(productRealEstateService.findAll());
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductRealEstate> getById(@PathVariable Integer id) {
+        return productRealEstateService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductRealEstate> create(@RequestBody ProductRealEstate productRealEstate) {
+        try {
+            return ResponseEntity.ok(productRealEstateService.save(productRealEstate));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductRealEstate> update(@PathVariable Integer id, @RequestBody ProductRealEstate productRealEstate) {
+        try {
+            return ResponseEntity.ok(productRealEstateService.update(id, productRealEstate));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        try {
+            productRealEstateService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
